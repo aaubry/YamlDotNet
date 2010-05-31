5775118c184c1e73d4c7d01f6868dd6f1c932b26
Antoine Aubry <aaubry@gmail.com>
2010-05-31 22:07:42 +0000

    Added: Version file.

--- gendoc.sh
+++ gendoc.sh
@@ -3,9 +3,9 @@ rm -rf doc/xml
 mkdir doc 2> /dev/null
 mkdir doc/xml 2> /dev/null
 mkdir doc/html 2> /dev/null
-monodocer -assembly YamlDotNet.Core/bin/Debug/YamlDotNet.Core.dll -importslashdoc YamlDotNet.Core/bin/Debug/YamlDotNet.Core.xml -path doc/xml
-monodocer -assembly YamlDotNet.RepresentationModel/bin/Debug/YamlDotNet.RepresentationModel.dll -importslashdoc YamlDotNet.RepresentationModel/bin/Debug/YamlDotNet.RepresentationModel.xml -path doc/xml
-monodocer -assembly YamlDotNet.Converters/bin/Debug/YamlDotNet.Converters.dll -importslashdoc YamlDotNet.Converters/bin/Debug/YamlDotNet.Converters.xml -path doc/xml
-monodocer -assembly YamlDotNet.Configuration/bin/Debug/YamlDotNet.Configuration.dll -importslashdoc YamlDotNet.Configuration/bin/Debug/YamlDotNet.Configuration.xml -path doc/xml
-monodocs2html -source doc/xml -dest doc/html
+monodocer YamlDotNet.Core/bin/Debug/YamlDotNet.Core.dll -i YamlDotNet.Core/bin/Debug/YamlDotNet.Core.xml -o doc/xml
+monodocer YamlDotNet.RepresentationModel/bin/Debug/YamlDotNet.RepresentationModel.dll -i YamlDotNet.RepresentationModel/bin/Debug/YamlDotNet.RepresentationModel.xml -o doc/xml
+monodocer YamlDotNet.Converters/bin/Debug/YamlDotNet.Converters.dll -i YamlDotNet.Converters/bin/Debug/YamlDotNet.Converters.xml -o doc/xml
+#monodocer YamlDotNet.Configuration/bin/Debug/YamlDotNet.Configuration.dll -i YamlDotNet.Configuration/bin/Debug/YamlDotNet.Configuration.xml -o doc/xml
+monodocs2html doc/xml -o doc/html
 
