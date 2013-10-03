3aa08a4e45ad5acb7a52b26f4d17f86fc1ef7f23
Roger Norling <roger.norling.se@gmail.com>
2013-10-03 16:08:10 +0200

    Merged the Core, Converters and RepresentationModel assemblies.
    Also renamed the DynamicYaml test assembly

---
+++ YamlDotNet/RepresentationModel/Serialization/YamlAliasAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace YamlDotNet.RepresentationModel.Serialization
+{
+	/// <summary>
+	/// Instructs the <see cref="Deserializer"/> to use a different field name for serialization.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+	public class YamlAliasAttribute : Attribute
+	{
+		/// <summary>
+		/// Gets or sets the alias name.
+		/// </summary>
+		public string Alias { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="YamlAliasAttribute" /> class.
+		/// </summary>
+		/// <param name="alias">The alias to use for this field.</param>
+		public YamlAliasAttribute(string alias)
+		{
+			Alias = alias;
+		}
+	}
+}
