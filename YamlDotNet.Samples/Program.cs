d6742c5f1260de5e73e2b918ffeaef0e49f1382f
Roger Norling <roger.norling.se@gmail.com>
2013-10-08 13:41:22 +0200

    Removed duplicate sample code.

--- YamlDotNet.Samples/Program.cs
+++ YamlDotNet.Samples/Program.cs
@@ -20,35 +20,43 @@
 //  SOFTWARE.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Reflection;
 
 namespace YamlDotNet.Samples
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var samples = Assembly.GetExecutingAssembly()
-                .GetTypes()
-                .Where(t => t != typeof(Program))
-                .Select(t => t.GetMethod("Run"))
-                .Where(m => m != null);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var samples = typeof(Program).Assembly.GetTypes()
+				.Where(t => Attribute.GetCustomAttribute(t, typeof(SampleAttribute)) != null)
+				.Select(t => t.GetMethod("Run")).Where(m => m != null);
 
-            foreach (var sample in samples)
-            {
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("Begin of sample {0}", sample.DeclaringType.Name);
-                Console.WriteLine("--------------------------------");
-                var target = Activator.CreateInstance(sample.DeclaringType);
-                sample.Invoke(target, new object[] { args });
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("End of sample {0}", sample.DeclaringType.Name);
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine();
-            }
-        }
-    }
+			foreach (var sample in samples)
+			{
+				Console.WriteLine("--------------------------------");
+				Console.WriteLine("Begin of sample {0}", NameOf(sample.DeclaringType));
+				Console.WriteLine("--------------------------------");
+
+				var target = Activator.CreateInstance(sample.DeclaringType);
+				sample.Invoke(target, new object[] { args });
+
+				Console.WriteLine("--------------------------------");
+				Console.WriteLine("End of sample {0}", NameOf(sample.DeclaringType));
+				Console.WriteLine("--------------------------------");
+				Console.WriteLine();
+			}
+			Console.ReadKey();
+		}
+
+		private static string NameOf(Type type)
+		{
+			return type != null? type.Name: "<Unknown>";
+		}
+	}
+
+	[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+	public sealed class SampleAttribute : Attribute
+	{
+	}
 }
