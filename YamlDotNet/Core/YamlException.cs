e580d328c4a5a4937155f992c84f71c54ec6853d
Felix Collins <felix@intranel.com>
2014-08-13 11:29:58 +1200

    Added Xamarin Android class library.

--- YamlDotNet/Core/YamlException.cs
+++ YamlDotNet/Core/YamlException.cs
@@ -109,7 +109,9 @@ namespace YamlDotNet.Core
 		/// 	<IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Read="*AllFiles*" PathDiscovery="*AllFiles*"/>
 		/// 	<IPermission class="System.Security.Permissions.SecurityPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Flags="SerializationFormatter"/>
 		/// </PermissionSet>
+#if !DROID
 		[SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
+#endif
 		public override void GetObjectData(SerializationInfo info, StreamingContext context)
 		{
 			base.GetObjectData(info, context);
