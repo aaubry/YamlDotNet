1a053945ccab2abaea01119dc40d702420e0e3d5
Antoine Aubry <aaubry@gmail.com>
2014-08-18 09:42:03 +0100

    Fix package version generation.

--- prebuild.ps1
+++ prebuild.ps1
@@ -3,5 +3,5 @@
 $buildNumber = ($env:APPVEYOR_BUILD_NUMBER, "0" -ne $null)[0]
 $version = Get-VersionFromTag
 
-Patch-Xml "YamlDotNet\YamlDotNet.nuspec" $version $buildNumber "/package/metadata/version/text()" @{ }
-Patch-AssemblyInfo "YamlDotNet\Properties\AssemblyInfo.cs" $version $buildNumber
+Patch-Xml "YamlDotNet\YamlDotNet.nuspec" $version "/package/metadata/version/text()" @{ }
+Patch-AssemblyInfo "YamlDotNet\Properties\AssemblyInfo.cs" $version
