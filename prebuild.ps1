771abe36899b2fc6e746b93611aae410ee86e211
Antoine Aubry <aaubry@gmail.com>
2014-09-02 23:19:33 +0100

    Try to set the build version from Powershell.

--- prebuild.ps1
+++ prebuild.ps1
@@ -3,5 +3,7 @@
 $buildNumber = ($env:APPVEYOR_BUILD_NUMBER, "0" -ne $null)[0]
 $version = Get-VersionFromTag
 
+Update-AppveyorBuild -Version "$version.$buildNumber"
+
 Patch-Xml "YamlDotNet\YamlDotNet.nuspec" $version "/package/metadata/version/text()" @{ }
 Patch-AssemblyInfo "YamlDotNet\Properties\AssemblyInfo.cs" $version
