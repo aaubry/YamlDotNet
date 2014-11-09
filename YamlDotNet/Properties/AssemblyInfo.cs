6946db2370ba28c889eda8149a4429c38045d9cd
Antoine Aubry <aaubry@gmail.com>
2014-11-09 22:53:09 +0000

    Configure the project assembly signing.

--- YamlDotNet/Properties/AssemblyInfo.cs
+++ YamlDotNet/Properties/AssemblyInfo.cs
@@ -57,9 +57,11 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
 [assembly: AssemblyFileVersion("0.0.0")]
 [assembly: AssemblyInformationalVersion("0.0.0")]
 
 [assembly: CLSCompliant(true)]
+#if !SIGNED
 [assembly: InternalsVisibleTo("YamlDotNet.Test")]
+#endif
