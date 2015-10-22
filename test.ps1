7c4600208e00566f40975409f6c4704fc7525d7e
Antoine Aubry <aaubry@gmail.com>
2015-10-22 13:43:31 +0100

    Add script to easily run unit tests.

---
+++ test.ps1
@@ -0,0 +1,13 @@
+
+nuget install xunit.runner.console -Version 2.1.0 -OutputDirectory packages
+
+@("Debug", "Release", "Release-Portable") | % {
+  Write-Host ""
+  Write-Host "Testing $_ build"
+  Write-Host "----------------------------------------------"
+
+  dir "YamlDotNet.Test\bin\$_\YamlDotNet.Test*dll" | % {
+    Write-Host $_.FullName
+    .\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe $_.FullName
+  }
+}
