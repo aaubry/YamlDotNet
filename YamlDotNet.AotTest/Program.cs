93626bc227811ba39224b21410c936ead120e851
Antoine Aubry <aaubry@gmail.com>
2016-02-03 15:05:32 +0000

    Add tests to detect AOT incompatibility.

--- YamlDotNet.AotTest/Program.cs
+++ YamlDotNet.AotTest/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using YamlDotNet.Serialization;
@@ -9,132 +8,88 @@ namespace YamlDotNet.AotTest
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            //var serializer = new Serializer();
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------------------"); 
+            Console.WriteLine();
 
-            //var data = new Model
-            //{
-            //    SingleItem = new Item<string> { Id = 1, Value = "single", },
-            //    ItemList = new List<Item<int>>
-            //    {
-            //        new Item<int> { Id = 2, Value = 0, },
-            //        new Item<int> { Id = 3, Value = 1, },
-            //        new Item<int> { Id = 4, Value = 2, },
-            //    },
-            //    ItemDictionary = new Dictionary<string, Item<string>>
-            //    {
-            //        { "item-0", new Item<string> { Id = 5, Value = "value-0" } },
-            //        { "item-1", new Item<string> { Id = 6, Value = "value-1" } },
-            //    },
-            //    Unknown = new Hashtable
-            //    {
-            //        { "scalar", new Item<int> { Id = 7, Value = 0, } },
-            //        { "array", new Item<int>[] { new Item<int> { Id = 8, Value = 1, } } },
-            //    },
-            //    ItemArray = new[]
-            //    {
-            //        new Item<long> { Id = 9, Value = 0, },
-            //        new Item<long> { Id = 10, Value = 1, },
-            //        new Item<long> { Id = 11, Value = 2, },
-            //    },
-            //};
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" Running AOT tests...");
+            Console.WriteLine();
 
-            //var buffer = new StringWriter();
+            TryDeserialize<MyDictionary>("DictionaryNodeDeserializer", "myDictionary: { winners: 3 }");
+            TryDeserialize<MyList>("CollectionNodeDeserializer", "myList: [ 1, 2, 3 ]");
+            TryDeserialize<MyArray>("ArayNodeDeserializer", "myArray: [ 1, 2, 3 ]");
 
-            //serializer.Serialize(buffer, data);
-            //Console.WriteLine(buffer.ToString());
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" {0} test succeeded, {1} tests failed", succeededTestCount, failedTestCount);
 
-            //var deserializer = new Deserializer();
-            //var deserializedData = deserializer.Deserialize<Model>(new StringReader(buffer.ToString()));
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine();
 
-            //Console.WriteLine(deserializedData.SingleItem);
-            //foreach (var item in deserializedData.ItemList)
-            //{
-            //    Console.WriteLine("- {0}", item);
-            //}
-            //foreach (var item in deserializedData.ItemDictionary)
-            //{
-            //    Console.WriteLine("{0}: {1}", item.Key, item.Value);
-            //}
-            //foreach (KeyValuePair<object, object> item in (IEnumerable)deserializedData.Unknown)
-            //{
-            //    Console.WriteLine("{0}: {1}", item.Key, item.Value);
-            //}
-            //foreach (var item in deserializedData.ItemArray)
-            //{
-            //    Console.WriteLine("- {0}", item);
-            //}
+            return failedTestCount;
+        }
 
-            //Console.WriteLine(typeof(Dictionary<string, Item<int>>).AssemblyQualifiedName);
+        private static int succeededTestCount;
+        private static int failedTestCount;
 
-//            var deserializer = new Deserializer();
-//            var deserialized = deserializer.Deserialize<object>(new StringReader(@"
-//                - !<!YamlDotNet.AotTest.ArrayContainer%601%5B%5BYamlDotNet.AotTest.Item%601%5B%5BSystem.String%2C%20mscorlib%2C%20Version%3D2.0.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3Db77a5c561934e089%5D%5D%2C%20YamlDotNet.AotTest%2C%20Version%3D1.0.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3Dnull%5D%5D%2C%20YamlDotNet.AotTest%2C%20Version%3D1.0.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3Dnull>
-//                  Items:
-//                    - Id: 1
-//                      Value: abc
-//                    - Id: 2
-//                      Value: def
-//                - !<!System.Collections.Generic.Dictionary%602%5B%5BSystem.String%2C%20mscorlib%2C%20Version%3D2.0.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3Db77a5c561934e089%5D%2C%5BYamlDotNet.AotTest.Item%601%5B%5BSystem.Int32%2C%20mscorlib%2C%20Version%3D2.0.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3Db77a5c561934e089%5D%5D%2C%20YamlDotNet.AotTest%2C%20Version%3D1.0.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3Dnull%5D%5D%2C%20mscorlib%2C%20Version%3D2.0.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3Db77a5c561934e089>
-//                  first:
-//                    Id: 1
-//                    Value: 10
-//                  second:
-//                    Id: 2
-//                    Value: 20
-//            "));
+        private static void TryDeserialize<T>(string testName, string yaml)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(" ");
+            Console.Write(testName.PadRight(70));
 
-//            Console.WriteLine();
-//            foreach (var item in (IEnumerable)deserialized)
-//            {
-//                Console.WriteLine(item.GetType().FullName);
-//            }
-//            Console.WriteLine();
+            var input = new StringReader(yaml);
+            var deserializer = new Deserializer(namingConvention: new CamelCaseNamingConvention());
+            try
+            {
+                deserializer.Deserialize<T>(input);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[success]");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                ++succeededTestCount;
+            }
+            catch (Exception ex)
+            {
+                var current = ex;
+                while (current != null)
+                {
+                    if (current is ExecutionEngineException)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[failure]");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(" ");
+                        Console.WriteLine(current.Message);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        ++failedTestCount;
+                        return;
+                    }
 
-//            var serializer = new Serializer();
-//            serializer.Serialize(Console.Out, deserialized);
-
-            string doc =
-@"myDictionary:
-    winners: 3";
-
-        var input = new StringReader(doc);
-        var deserializer = new Deserializer(namingConvention: new CamelCaseNamingConvention());
-        var order = deserializer.Deserialize<MyObject>(input);
-
-        Console.WriteLine("winners: " + order.myDictionary["winners"]);
+                    current = current.InnerException;
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                throw;
+            }
         }
     }
 
-    public class MyObject
+    public class MyDictionary
     {
         public Dictionary<string, int> myDictionary { get; set; }
     }
 
-    public class ArrayContainer<T>
+    public class MyList
     {
-        public T[] Items { get; set; }
+        public List<int> myList { get; set; }
     }
 
-    public class Model
+    public class MyArray
     {
-        public Item<string> SingleItem { get; set; }
-        public List<Item<int>> ItemList { get; set; }
-        public Dictionary<string, Item<string>> ItemDictionary { get; set; }
-        public object Unknown { get; set; }
-        public Item<long>[] ItemArray { get; set; }
-    }
-
-    public class Item<T>
-    {
-        public int Id { get; set; }
-
-        public T Value { get; set; }
-
-        public override string ToString()
-        {
-            return string.Format("{0}: {1}", Id, Value);
-        }
+        public int[] myArray { get; set; }
     }
 }
