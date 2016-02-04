34a58f26df59c69e384926cb5302716246919e8e
Nate Barbettini <nathanaelb@gmail.com>
2016-02-04 15:14:05 -0800

    Mark YamlVisitor as obsolete

--- YamlDotNet/RepresentationModel/YamlVisitor.cs
+++ YamlDotNet/RepresentationModel/YamlVisitor.cs
@@ -1,16 +1,16 @@
 //  This file is part of YamlDotNet - A .NET library for YAML.
 //  Copyright (c) Antoine Aubry and contributors
-    
+
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of
 //  this software and associated documentation files (the "Software"), to deal in
 //  the Software without restriction, including without limitation the rights to
 //  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
 //  of the Software, and to permit persons to whom the Software is furnished to do
 //  so, subject to the following conditions:
-    
+
 //  The above copyright notice and this permission notice shall be included in all
 //  copies or substantial portions of the Software.
-    
+
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,6 +26,7 @@ namespace YamlDotNet.RepresentationModel
     /// <summary>
     /// Abstract implementation of <see cref="IYamlVisitor"/> that knows how to walk a complete Yaml object model.
     /// </summary>
+    [Obsolete("Use YamlVisitorBase")]
     public abstract class YamlVisitor : IYamlVisitor {
         /// <summary>
         /// Called when this object is visiting a <see cref="YamlStream"/>.
@@ -135,7 +136,7 @@ namespace YamlDotNet.RepresentationModel
         {
             // Do nothing.
         }
-        
+
         /// <summary>
         /// Visits every child of a <see cref="YamlStream"/>.
         /// </summary>
