79ecaa671c4f850586ee6e7693c38e614efe79b0
Antoine Aubry <aaubry@gmail.com>
2016-09-07 23:57:53 +0100

    When building a pre-release version, use a greater version number than the current one.

--- prebuild.ps1
+++ prebuild.ps1
@@ -1,13 +1,14 @@
 . .\BuildUtils\build_utils.ps1
 
 $buildNumber = ($env:APPVEYOR_BUILD_NUMBER, "0" -ne $null)[0]
-$version = Get-VersionFromTag
-
-Update-AppveyorBuild -Version "$version.$buildNumber"
 
 if($env:APPVEYOR_REPO_BRANCH -ne "release") {
-    $version = "$version-pre$buildNumber"
+	$version = Get-VersionFromTag $buildNumber
+} else {
+	$version = Get-VersionFromTag
 }
+
+Update-AppveyorBuild -Version $version
 
 Patch-Xml "YamlDotNet\YamlDotNet.Unsigned.nuspec" $version "/package/metadata/version/text()" @{ }
 Patch-Xml "YamlDotNet\YamlDotNet.Signed.nuspec" $version "/package/metadata/version/text()" @{ }
