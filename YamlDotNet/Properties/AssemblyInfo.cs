bf26803f82a5dbb2b8936599a6ae845fc72ad09d
Nate Barbettini <nathanaelb@gmail.com>
2017-03-29 15:10:37 -0700

    Working build

---
+++ YamlDotNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+#if !SIGNED
+#if PORTABLE
+[assembly: InternalsVisibleTo("YamlDotNet.Test.Portable")]
+#else
+[assembly: InternalsVisibleTo("YamlDotNet.Test")]
+#endif
+#endif
