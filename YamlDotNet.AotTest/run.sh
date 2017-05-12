716391420ec657b92c8469db2631ebae0aa3eea4
Antoine Aubry <antoine@aaubry.net>
2017-05-13 00:19:14 +0100

    Ensure that the documentation can be build with docker

--- YamlDotNet.AotTest/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-xbuild /p:Configuration=Debug-AOT /t:Rebuild YamlDotNet.sln
-mono --aot=full YamlDotNet.AotTest/bin/Debug/YamlDotNet.dll
-mono --aot=full YamlDotNet.AotTest/bin/Debug/YamlDotNet.AotTest.exe
-mono --full-aot YamlDotNet.AotTest/bin/Debug/YamlDotNet.AotTest.exe
--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -u=`id -u` -v `pwd`:/build -w /build -it aaubry/yamldotnet.local ./build.sh "$@"
+if [ -d ../YamlDotNet.wiki ]
+  then
+    docker run -u=`id -u` -v `pwd`:/build/YamlDotNet -v `pwd`/../YamlDotNet.wiki:/build/YamlDotNet.wiki -w /build/YamlDotNet -it aaubry/yamldotnet.local ./build.sh "$@"
+  else
+    docker run -u=`id -u` -v `pwd`:/build/YamlDotNet -w /build/YamlDotNet -it aaubry/yamldotnet.local ./build.sh "$@"
+fi
