1435cba173c5c9664a2a86b60183ebcd976c4fbb
Jos Hickson <j.hickson@winton.com>
2018-02-13 14:58:18 +0000

    Use System.Text.UTF7Encoding on NET Standard 1.3
    This is available from System.Text.Encoding.Extensions.
    
    I also removed the double use of UTF8Encoding in IsUnicode.

--- YamlDotNet/Helpers/Portability.cs
+++ YamlDotNet/Helpers/Portability.cs
@@ -42,42 +42,6 @@ namespace YamlDotNet
 #endif
 
 #if NETSTANDARD1_3
-    /// <summary>
-    /// Mock UTF7Encoding to avoid having to add #if all over the place
-    /// </summary>
-    internal sealed class UTF7Encoding : System.Text.Encoding
-    {
-        public override int GetByteCount(char[] chars, int index, int count)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetCharCount(byte[] bytes, int index, int count)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetChars(byte[] bytes, int byteIndex, int byteCount, char[] chars, int charIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetMaxByteCount(int charCount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetMaxCharCount(int byteCount)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     internal static class ReflectionExtensions
     {
         public static Type BaseType(this Type type)
