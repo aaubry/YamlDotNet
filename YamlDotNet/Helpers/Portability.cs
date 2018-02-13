d7017d466aa7e52193efe0d7af6e0b1142770381
Jos Hickson <j.hickson@winton.com>
2018-02-13 14:52:21 +0000

    Use System.TypeCode for NET Standard 1.3
    For some reason TypeCode.DBNull was only reintroduced for NET Standard
    2.0 and is not in the System.Runtime platform extension for NET Standard
    1.3. So, in FullObjectGraphTraversalStrategy I have handled DBNull
    differently to before.

--- YamlDotNet/Helpers/Portability.cs
+++ YamlDotNet/Helpers/Portability.cs
@@ -277,28 +277,6 @@ namespace YamlDotNet
         }
     }
 
-    internal enum TypeCode
-    {
-        Empty = 0,
-        Object = 1,
-        DBNull = 2,
-        Boolean = 3,
-        Char = 4,
-        SByte = 5,
-        Byte = 6,
-        Int16 = 7,
-        UInt16 = 8,
-        Int32 = 9,
-        UInt32 = 10,
-        Int64 = 11,
-        UInt64 = 12,
-        Single = 13,
-        Double = 14,
-        Decimal = 15,
-        DateTime = 16,
-        String = 18,
-    }
-
     internal sealed class CultureInfoAdapter : CultureInfo
     {
         private readonly IFormatProvider _provider;
