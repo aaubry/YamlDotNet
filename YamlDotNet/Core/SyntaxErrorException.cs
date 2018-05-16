145477a8065214813898bbe75fb58d3a503d3d73
Antoine Aubry <antoine@aaubry.net>
2018-05-16 09:42:06 +0100

    Remove serialization support from exceptions

--- YamlDotNet/Core/SyntaxErrorException.cs
+++ YamlDotNet/Core/SyntaxErrorException.cs
@@ -20,7 +20,6 @@
 //  SOFTWARE.
 
 using System;
-using System.Runtime.Serialization;
 
 namespace YamlDotNet.Core
 {
@@ -63,19 +62,5 @@ namespace YamlDotNet.Core
             : base(message, inner)
         {
         }
-
-#if !(NETSTANDARD1_3 || UNITY)
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SyntaxErrorException"/> class.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
-        /// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is null. </exception>
-        /// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is null or <see cref="P:System.Exception.HResult"/> is zero (0). </exception>
-        protected SyntaxErrorException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
-#endif
     }
 }
--- YamlDotNet/Core/YamlException.cs
+++ YamlDotNet/Core/YamlException.cs
@@ -20,7 +20,6 @@
 //  SOFTWARE.
 
 using System;
-using System.Runtime.Serialization;
 
 namespace YamlDotNet.Core
 {
@@ -83,39 +82,5 @@ namespace YamlDotNet.Core
             : base(message, inner)
         {
         }
-
-#if !(NETSTANDARD1_3 || UNITY)
-        /// <summary>
-        /// Initializes a new instance of the <see cref="YamlException"/> class.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
-        /// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is null. </exception>
-        /// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is null or <see cref="P:System.Exception.HResult"/> is zero (0). </exception>
-        protected YamlException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-            Start = (Mark)info.GetValue("Start", typeof(Mark));
-            End = (Mark)info.GetValue("End", typeof(Mark));
-        }
-
-        /// <summary>
-        /// When overridden in a derived class, sets the <see cref="T:System.Runtime.Serialization.SerializationInfo"/> with information about the exception.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
-        /// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is a null reference (Nothing in Visual Basic). </exception>
-        /// <PermissionSet>
-        ///     <IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Read="*AllFiles*" PathDiscovery="*AllFiles*"/>
-        ///     <IPermission class="System.Security.Permissions.SecurityPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Flags="SerializationFormatter"/>
-        /// </PermissionSet>
-        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.LinkDemand, Flags = System.Security.Permissions.SecurityPermissionFlag.SerializationFormatter)]
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-            info.AddValue("Start", Start);
-            info.AddValue("End", End);
-        }
-#endif
     }
 }
