e6c355dabd6e2a36ae8df8f1cab8b8c50dbb1b02
Antoine Aubry <antoine@gothelist.com>
2019-08-09 12:05:56 +0100

    Add netcoreapp3.0 and netstandard2.1 target frameworks

--- YamlDotNet/Core/CharacterAnalyzer.cs
+++ YamlDotNet/Core/CharacterAnalyzer.cs
@@ -21,7 +21,6 @@
 
 using System;
 using System.Diagnostics;
-using System.Linq;
 
 namespace YamlDotNet.Core
 {
--- YamlDotNet/Serialization/Utilities/TypeConverter.cs
+++ YamlDotNet/Serialization/Utilities/TypeConverter.cs
@@ -42,7 +42,11 @@ namespace YamlDotNet.Serialization.Utilities
         /// </summary>
         /// <typeparam name="TConvertible">The type to which the converter should be associated.</typeparam>
         /// <typeparam name="TConverter">The type of the converter.</typeparam>
+#endif
+#if !(NETCOREAPP3_0 || NETSTANDARD2_1 || NETSTANDARD1_3 || UNITY)
         [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.LinkDemand, Name = "FullTrust")]
+#endif
+#if !(NETSTANDARD1_3 || UNITY)
         public static void RegisterTypeConverter<TConvertible, TConverter>()
             where TConverter : System.ComponentModel.TypeConverter
         {
