a980a34d70f134c1baa6b676d0f83cf22f0da33b
Giedo Donkers <giedo.donkers@via.nl>
2021-06-12 12:22:22 +0200

    Tested best implementation of `DateTimeStyles.RoundtripKind`

--- YamlDotNet/Serialization/NodeDeserializers/ScalarNodeDeserializer.cs
+++ YamlDotNet/Serialization/NodeDeserializers/ScalarNodeDeserializer.cs
@@ -91,10 +91,7 @@ namespace YamlDotNet.Serialization.NodeDeserializers
 
                 case TypeCode.DateTime:
                     // TODO: This is probably incorrect. Use the correct regular expression.
-                    var style = scalar.Value.EndsWith("Z")
-                        ? DateTimeStyles.RoundtripKind
-                        : scalar.Value.Length > 27 ? DateTimeStyles.AssumeLocal : DateTimeStyles.None;
-                    value = DateTime.Parse(scalar.Value, CultureInfo.InvariantCulture, style);
+                    value = DateTime.Parse(scalar.Value, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
                     break;
 
                 default:
