3afceafd2794a9cf81dedfd2ce80e76724ab07d3
cp <cp>
2022-01-07 09:53:47 +0100

    Fix inline comment emission

--- YamlDotNet/Core/Emitter.cs
+++ YamlDotNet/Core/Emitter.cs
@@ -654,7 +654,7 @@ namespace YamlDotNet.Core
 
             if (comment.IsInline)
             {
-                Write(' ');
+                Write(" # ");
                 Write(string.Join(" ", lines));
             }
             else
