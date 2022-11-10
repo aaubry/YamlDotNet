e8cb22e8f0c5ab050611cfbf04ef381fbb6ab50e
Adam Ralph <adam@adamralph.com>
2022-11-10 12:33:43 +0100

    updated Bullseye from 4.0.0 to 4.2.0

--- tools/build/Program.cs
+++ tools/build/Program.cs
@@ -9,9 +9,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Bullseye;
-using Bullseye.Internal;
 using static Bullseye.Targets;
-using OperatingSystem = Bullseye.Internal.OperatingSystem;
 
 namespace build
 {
@@ -136,18 +134,18 @@ namespace build
 
             var (specifiedTargets, options, unknownOptions, showHelp) = CommandLine.Parse(filteredArguments);
             verbose = options.Verbose;
-            Host = options.Host.DetectIfAutomatic();
+            Host = options.Host.DetectIfNull();
 
-            var operatingSystem =
+            var osPlatform =
                 RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-                    ? OperatingSystem.Windows
+                    ? OSPlatform.Windows
                     : RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
-                        ? OperatingSystem.Linux
+                        ? OSPlatform.Linux
                         : RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
-                            ? OperatingSystem.MacOS
-                            : OperatingSystem.Unknown;
+                            ? OSPlatform.OSX
+                            : OSPlatform.Create("Unknown");
 
-            palette = new Palette(options.NoColor, options.NoExtendedChars, options.Host, operatingSystem);
+            palette = new Palette(options.NoColor, options.NoExtendedChars, Host, osPlatform);
 
             var targets = specifiedTargets.ToList();
 
@@ -209,9 +207,9 @@ namespace build
             if (showHelp)
             {
                 Console.WriteLine();
-                Console.WriteLine($"{palette.Default}Additional options:");
-                Console.WriteLine($"  {palette.Option}--no-prerelease            {palette.Default}Force the current version to be considered final{palette.Reset}");
-                Console.WriteLine($"  {palette.Option}--version=<version>        {palette.Default}Force the current version to equal to the specified value{palette.Reset}");
+                Console.WriteLine($"{palette.Text}Additional options:");
+                Console.WriteLine($"  {palette.Option}--no-prerelease            {palette.Text}Force the current version to be considered final{palette.Default}");
+                Console.WriteLine($"  {palette.Option}--version=<version>        {palette.Text}Force the current version to equal to the specified value{palette.Default}");
             }
 
             return exitCode;
@@ -227,12 +225,12 @@ namespace build
 
         public static void WriteInformation(string text)
         {
-            Console.WriteLine($"  {palette.Default}(i)  {text}{palette.Reset}");
+            Console.WriteLine($"  {palette.Text}(i)  {text}{palette.Default}");
         }
 
         public static void WriteWarning(string text)
         {
-            Console.WriteLine($"  {palette.Option}/!\\  {text}{palette.Reset}");
+            Console.WriteLine($"  {palette.Option}/!\\  {text}{palette.Default}");
         }
 
         public static void WriteImportant(string text)
@@ -300,7 +298,7 @@ namespace build
 
         private static void Write(string text, string color)
         {
-            Console.WriteLine($"{color}{text}{palette.Reset}");
+            Console.WriteLine($"{color}{text}{palette.Default}");
         }
 
         public static IEnumerable<string> ReadLines(string name, string? args = null, string? workingDirectory = null) => SimpleExec.Command
