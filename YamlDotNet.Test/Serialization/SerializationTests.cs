afdd1e2600bd1f255b763c6100ad4faa5bafdcf0
Chris Nussbaum <chris.nussbaum@protolabs.com>
2022-12-02 14:03:04 -0600

    Fix IgnoreExtraPropertiesIfWantedNamingScheme test

--- YamlDotNet.Test/Serialization/SerializationTests.cs
+++ YamlDotNet.Test/Serialization/SerializationTests.cs
@@ -1312,7 +1312,6 @@ y:
                     "scratch: 'scratcher'",
                     "deleteScratch: false",
                     "notScratch: 9443",
-                    "notScratch: 192.168.1.30",
                     "mappedScratch:",
                     "- '/work/'"
                 );
