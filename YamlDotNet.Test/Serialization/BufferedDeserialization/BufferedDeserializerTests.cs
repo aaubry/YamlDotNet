a9b28e993efd09f2b3089d9aafacc543843a9f3c
Lucas <lucas.sargent@outlook.com>
2023-02-06 03:01:37 +0000

    extend tests

--- YamlDotNet.Test/Serialization/BufferedDeserialization/BufferedDeserializerTests.cs
+++ YamlDotNet.Test/Serialization/BufferedDeserialization/BufferedDeserializerTests.cs
@@ -10,9 +10,28 @@ using YamlDotNet.Serialization.NamingConventions;
 namespace YamlDotNet.Test.Serialization.BufferedDeserialization
 {
     public class BufferedDeserializerTest
-    {   
+    {
         [Fact]
-        public void KeyValueTypeDiscriminator_WithParentBaseType()
+        public void KeyValueTypeDiscriminator_WithParentBaseType_Single()
+        {
+            var bufferedDeserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .WithBufferedNodeDeserializer(options => {
+                    options.AddKeyValueTypeDiscriminator<KubernetesResource>(
+                        "kind",
+                        ("Namespace", typeof(KubernetesNamespace)),
+                        ("Service", typeof(KubernetesService)));
+                    },
+                    maxDepth: 3,
+                    maxLength: 40)
+                .Build();
+
+            var service = bufferedDeserializer.Deserialize<KubernetesResource>(KubernetesServiceYaml);
+            service.Should().BeOfType<KubernetesService>();
+        }
+
+        [Fact]
+        public void KeyValueTypeDiscriminator_WithParentBaseType_List()
         {
             var bufferedDeserializer = new DeserializerBuilder()
                 .WithNamingConvention(CamelCaseNamingConvention.Instance)
@@ -30,26 +49,85 @@ namespace YamlDotNet.Test.Serialization.BufferedDeserialization
             resources[0].Should().BeOfType<KubernetesNamespace>();
             resources[1].Should().BeOfType<KubernetesService>();
         }
+        
+        [Fact]
+        public void KeyValueTypeDiscriminator_WithObjectBaseType_Single()
+        {
+            var bufferedDeserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .WithBufferedNodeDeserializer(options => {
+                    options.AddKeyValueTypeDiscriminator<object>(
+                        "kind",
+                        ("Namespace", typeof(KubernetesNamespace)),
+                        ("Service", typeof(KubernetesService)));
+                    },
+                    maxDepth: 3,
+                    maxLength: 40)
+                .Build();
 
-        public const string KubernetesNamespaceYaml = @"
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: test-namespace
-";
-        public const string KubernetesServiceYaml = @"
-apiVersion: v1
-kind: Service
-metadata:
-  name: my-service
-spec:
-  selector:
-    app.kubernetes.io/name: MyApp
-  ports:
-    - protocol: TCP
-      port: 80
-      targetPort: 9376
-";
+            var service = bufferedDeserializer.Deserialize<object>(KubernetesServiceYaml);
+            service.Should().BeOfType<KubernetesService>();
+        }
+
+        [Fact]
+        public void KeyValueTypeDiscriminator_WithObjectBaseType_List()
+        {
+            var bufferedDeserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .WithBufferedNodeDeserializer(options => {
+                    options.AddKeyValueTypeDiscriminator<object>(
+                        "kind",
+                        ("Namespace", typeof(KubernetesNamespace)),
+                        ("Service", typeof(KubernetesService)));
+                    },
+                    maxDepth: 3,
+                    maxLength: 30)
+                .Build();
+
+            var resources = bufferedDeserializer.Deserialize<List<object>>(ListOfKubernetesYaml);
+            resources[0].Should().BeOfType<KubernetesNamespace>();
+            resources[1].Should().BeOfType<KubernetesService>();
+        }
+
+        [Fact]
+        public void KeyValueTypeDiscriminator_WithInterfaceBaseType_Single()
+        {
+            var bufferedDeserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .WithBufferedNodeDeserializer(options => {
+                    options.AddKeyValueTypeDiscriminator<IKubernetesResource>(
+                        "kind",
+                        ("Namespace", typeof(KubernetesNamespace)),
+                        ("Service", typeof(KubernetesService)));
+                    },
+                    maxDepth: 3,
+                    maxLength: 40)
+                .Build();
+
+            var service = bufferedDeserializer.Deserialize<IKubernetesResource>(KubernetesServiceYaml);
+            service.Should().BeOfType<KubernetesService>();
+        }
+
+        [Fact]
+        public void KeyValueTypeDiscriminator_WithInterfaceBaseType_List()
+        {
+            var bufferedDeserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .WithBufferedNodeDeserializer(options => {
+                    options.AddKeyValueTypeDiscriminator<IKubernetesResource>(
+                        "kind",
+                        ("Namespace", typeof(KubernetesNamespace)),
+                        ("Service", typeof(KubernetesService)));
+                    },
+                    maxDepth: 3,
+                    maxLength: 30)
+                .Build();
+
+            var resources = bufferedDeserializer.Deserialize<List<IKubernetesResource>>(ListOfKubernetesYaml);
+            resources[0].Should().BeOfType<KubernetesNamespace>();
+            resources[1].Should().BeOfType<KubernetesService>();
+        }
+
         public const string ListOfKubernetesYaml = @"
 - apiVersion: v1
   kind: Namespace
@@ -68,7 +146,23 @@ spec:
         targetPort: 9376
 ";
 
-        public class KubernetesResource
+        public const string KubernetesServiceYaml = @"
+apiVersion: v1
+kind: Service
+metadata:
+  name: my-service
+spec:
+  selector:
+    app.kubernetes.io/name: MyApp
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 9376
+";
+
+        public interface IKubernetesResource { }
+
+        public class KubernetesResource : IKubernetesResource
         {
             public string ApiVersion { get; set; }
             public string Kind { get; set; }
