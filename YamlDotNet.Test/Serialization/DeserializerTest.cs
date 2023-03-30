10a66689ef747c802e642ce98e31419d52d6fd05
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2023-03-30 00:25:49 -0300

    Fixes test `DeserializeScalarEdgeCases` for non english cultures.
    The test fails if the machine culture does not use the
    dot decimal separator. This is caused due to `object.ToString()` being
    used that is culture sensitive. This commit fixes the test using
    `string.Format` with `CultureInfo.InvariantCulture`

--- YamlDotNet.Test/Serialization/DeserializerTest.cs
+++ YamlDotNet.Test/Serialization/DeserializerTest.cs
@@ -28,6 +28,7 @@ using Xunit;
 using YamlDotNet.Core;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
+using System.Globalization;
 
 namespace YamlDotNet.Test.Serialization
 {
@@ -279,7 +280,8 @@ b: &number 1
         public void DeserializeScalarEdgeCases(IConvertible value, Type type)
         {
             var deserializer = new DeserializerBuilder().Build();
-            var result = deserializer.Deserialize(value.ToString(), type);
+            var stringValue = string.Format(CultureInfo.InvariantCulture, "{0}", value);
+            var result = deserializer.Deserialize(stringValue, type);
 
             result.Should().Be(value);
         }
