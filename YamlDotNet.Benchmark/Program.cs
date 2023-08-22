094bad6a7076126c097f41d8954812c74f7b2f2d
Edward Cooke <edward@frakkingsweet.com>
2023-08-21 22:38:14 -0600

    Added a DateTimeOffsetConverter with a sample on usage

--- YamlDotNet.Benchmark/Program.cs
+++ YamlDotNet.Benchmark/Program.cs
@@ -1,4 +1,11 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System.Globalization;
+using BenchmarkDotNet.Running;
 using YamlDotNet.Benchmark;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
-BenchmarkSwitcher.FromAssembly(typeof(YamlStreamBenchmark).Assembly).Run(args);
+var dateTimeOffset = new DateTimeOffset(new DateTime(2017, 1, 2, 3, 4, 5), new TimeSpan(-6, 0, 0));
+Console.WriteLine(dateTimeOffset.ToString("MM/dd/yyyy HH:mm:ss zzz", CultureInfo.InvariantCulture));
+Console.WriteLine(dateTimeOffset.ToString("O", CultureInfo.InvariantCulture));
