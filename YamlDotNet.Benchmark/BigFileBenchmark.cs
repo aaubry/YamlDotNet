d9b7e0f370fe5c444e99f12322e0b0444aeaf0b1
Edward Cooke <edward@frakkingsweet.com>
2024-07-05 23:38:33 -0600

    Added large yaml file benchmark

---
+++ YamlDotNet.Benchmark/BigFileBenchmark.cs
@@ -0,0 +1,54 @@
+﻿// This file is part of YamlDotNet - A .NET library for YAML.
+// Copyright (c) Antoine Aubry and contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.IO.Compression;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using FastSerialization;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+
+namespace YamlDotNet.Benchmark;
+
+[MemoryDiagnoser]
+public class BigFileBenchmark
+{
+    private string yamlString = "";
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var stringBuilder = new StringBuilder();
+        //100mb
+        while (stringBuilder.Length < 1000 * 1000 * 100)
+        {
+            stringBuilder.AppendLine("- test");
+        }
+        yamlString = stringBuilder.ToString();
+    }
+
+    [Benchmark]
+    public void LoadLarge()
+    {
+        var deserializer = new DeserializerBuilder().Build();
+        var result = deserializer.Deserialize<List<string>>(yamlString);
+    }
+}
--- YamlDotNet.Benchmark/Program.cs
+++ YamlDotNet.Benchmark/Program.cs
@@ -1,4 +1,16 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System.Diagnostics;
 using YamlDotNet.Benchmark;
 
-BenchmarkSwitcher.FromAssembly(typeof(YamlStreamBenchmark).Assembly).Run(args);
+//BenchmarkSwitcher.FromAssembly(typeof(YamlStreamBenchmark).Assembly).Run(args);
+var x = new BigFileBenchmark();
+x.Setup();
+Console.WriteLine("Warming up with a single run");
+x.LoadLarge();
+Console.WriteLine("Done, executing attempts");
+while (true)
+{
+    var stop = new Stopwatch();
+    stop.Start();
+    x.LoadLarge();
+    Console.WriteLine($"{DateTime.Now} - Loaded - Elapsed: ${stop.Elapsed}");
+}
