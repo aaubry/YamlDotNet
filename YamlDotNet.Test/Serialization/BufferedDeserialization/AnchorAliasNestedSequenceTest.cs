d2b4d45d1af4096e28847fbc9295546dc33ae9b6
Legoshi <entityparrot@qq.com>
2025-02-28 14:01:03 +0800

    Add two test about anchor

---
+++ YamlDotNet.Test/Serialization/BufferedDeserialization/AnchorAliasNestedSequenceTest.cs
@@ -0,0 +1,107 @@
+﻿// This file is part of YamlDotNet - A .NET library for YAML.
+// Copyright (c) Antoine Aubry and contributors
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace YamlDotNet.Test.Serialization.BufferedDeserialization;
+
+public class AnchorAliasNestedSequenceTest
+{
+    [Fact]
+    public void AnchorAliasNestedSequence()
+    {
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .WithTypeConverter(FilterCollectionConverter.Instance)
+            .Build();
+        var executors = deserializer.Deserialize<List<Executor>>(Document);
+        executors[0].Filters.Count.Should().Be(2);
+        executors[1].Filters.Count.Should().Be(3);
+    }
+
+    public class FilterCollectionConverter : IYamlTypeConverter
+    {
+        public static IYamlTypeConverter Instance { get; } = new FilterCollectionConverter();
+
+        public bool Accepts(Type type)
+        {
+            return type == typeof(List<Filter>);
+        }
+
+        public object ReadYaml(IParser parser, Type type, ObjectDeserializer rootDeserializer)
+        {
+            var result = new List<Filter>();
+            parser.Consume<SequenceStart>();
+            while (!parser.TryConsume<SequenceEnd>(out _))
+            {
+                if (parser.TryConsume<SequenceStart>(out _))
+                {
+                    while (!parser.TryConsume<SequenceEnd>(out _))
+                    {
+                        result.Add((Filter)rootDeserializer.Invoke(typeof(Filter))!);
+                    }
+                }
+
+                result.Add((Filter)rootDeserializer.Invoke(typeof(Filter))!);
+            }
+
+            return result;
+        }
+
+        public void WriteYaml(IEmitter emitter, object value, Type type, ObjectSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public const string Document = @"
+- name: a
+  filters: &shared
+    - type: foo
+      value: 1
+    - type: bar
+      value: 2
+- name: b
+  filters: 
+    - *shared
+    - type: extra
+      value: 3
+";
+
+    public class Executor
+    {
+        public string Name { get; set; }
+        public List<Filter> Filters { get; set; }
+    }
+
+    public class Filter
+    {
+        public string Type { get; set; }
+        public int Value { get; set; }
+    }
+}
